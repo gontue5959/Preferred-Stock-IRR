3e0d747389c8b02219280f4cc780078e3c077805
gontue5959 <49179777+gontue5959@users.noreply.github.com>
2019-11-11 21:48:20 +0800

    Create function_irr.py

---
+++ function_irr.py
@@ -0,0 +1,21 @@
+ #xirr計算函數 https://github.com/dkensinger/python/blob/master/XIRR.py
+def xirr(transactions):
+    years = [(ta[0] - transactions[0][0]).days / 365.0 for ta in transactions]
+    residual = 1
+    step = 0.05
+    guess = 0.05
+    epsilon = 0.0001
+    limit = 10000
+    while abs(residual) > epsilon and limit > 0:
+        limit -= 1
+        residual = 0.0
+        for i, ta in enumerate(transactions):
+            residual += ta[1] / pow(guess, years[i])
+        if abs(residual) > epsilon:
+            if residual > 0:
+                guess += step
+            else:
+                guess -= step
+                step /= 2.0
+    return guess-1
+    
